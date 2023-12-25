4ca3a2393e24334797709897ce0b8ef4dbddc3ec
Anne Macedo <7549205+retpolanne@users.noreply.github.com>
2023-12-25 09:18:15 -0300

    Add a bunch of things

--- boot.sh
+++ boot.sh
@@ -8,30 +8,35 @@ MEM=2048
 
 if [ "$CDROM" ]; then
        	CDFLAG="-drive file=$CDROM,format=raw,media=cdrom"
-	BOOT=d
 fi
 
-echo $CDROM | grep -i "os9" && MEM=512 && MACH="mac99"
+echo $CDROM | grep -i "os9" && MEM=512 && MACH="mac99" && BOOT=d
 echo $HDA | grep -i "os9" && MEM=512 && MACH="mac99"
 
 echo qemu-system-ppc \
 	-boot $BOOT \
 	-M $MACH \
 	-m $MEM \
+	-L /opt/homebrew/Cellar/qemu/8.1.1/share/qemu \
 	-prom-env 'boot-args=-v' \
 	-device VGA,edid=on \
 	-prom-env "vga-ndrv?=true" \
 	-g 1440x900x32 \
+	-accel tcg,tb-size=2048 \
 	-netdev user,id=network0 -device sungem,netdev=network0 \
+	-usb -device usb-mouse -device usb-kbd \
 	-drive file=$HDA,format=raw,media=disk $CDFLAG $BOOTARG
 
 qemu-system-ppc \
 	-boot $BOOT \
 	-M $MACH \
 	-m $MEM \
+	-L /opt/homebrew/Cellar/qemu/8.1.1/share/qemu \
 	-prom-env 'boot-args=-v' \
-	-device VGA,edid=on \
+	-device VGA,edid=on,vgamem_mb=64,xres=1440,yres=900 \
 	-prom-env "vga-ndrv?=true" \
 	-g 1440x900x32 \
+	-accel tcg,tb-size=2048 \
 	-netdev user,id=network0 -device sungem,netdev=network0 \
+	-usb -device usb-mouse -device usb-kbd \
 	-drive file=$HDA,format=raw,media=disk $CDFLAG $BOOTARG
